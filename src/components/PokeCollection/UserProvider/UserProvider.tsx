8b0a05809616e28cf82acefcc1cd8b9fe1ea6408
Jeffcar1993 <jphoenix.07@hotmail.com>
2024-10-06 15:22:43 -0500

    UPDATE favorite, search and userProvider component for add functionality

--- src/components/PokeCollection/UserProvider/UserProvider.tsx
+++ src/components/PokeCollection/UserProvider/UserProvider.tsx
@@ -3,18 +3,34 @@ import * as React from "react";
 type UserContextType = {
   username: string;
   setUsername: (username: string) => void;
+  favorites: number[];
+  toggleFavorite: (pokemonId: number) => void;
 }
 
 export const UserContext = React.createContext<UserContextType | undefined>(undefined);
 
 export function UserProvider({ children } : { children: React.ReactNode }) {
   const [username, setUsername] = React.useState<string>("");
- 
+  const [favorites, setFavorites] = React.useState<number[]>([]);
+
+  const toggleFavorite = (pokemonId: number) => {
+    setFavorites(prev =>
+      prev.includes(pokemonId)
+        ? prev.filter(id => id !== pokemonId) // Remover si ya está
+        : [...prev, pokemonId] // Agregar si no está
+    );
+  };
+
   return (
-    <UserContext.Provider value={{ username, setUsername }}>
+    <UserContext.Provider
+      value={{
+        username,
+        setUsername,
+        favorites,
+        toggleFavorite,
+      }}
+    >
       {children}
     </UserContext.Provider>
   );
 }
-
-  
