1ef020b11eb0db3ff9d4dad3fb22fb699675dd92
Dana Gamez <danagamezdbg@gmail.com>
2024-10-07 00:06:43 -0600

    FeedVideo

---
+++ src/VideoPlayer.tsx
@@ -0,0 +1,36 @@
+// src/VideoPlayer.tsx
+import React from 'react';
+import ReactPlayer from 'react-player';
+
+interface VideoPlayerProps {
+  url: string;
+  playing: boolean; // Corregido de "boolen" a "boolean"
+  volume: number;
+  playbackRate: number;
+  onPlay: () => void;
+  onPause: () => void;
+}
+
+const VideoPlayer: React.FC<VideoPlayerProps> = ({
+  url,
+  playing,
+  volume,
+  playbackRate,
+  onPlay,
+  onPause,
+}) => {
+  return (
+    <ReactPlayer
+      url={url}
+      playing={playing}
+      volume={volume}
+      playbackRate={playbackRate}
+      onPlay={onPlay}
+      onPause={onPause}
+      width="100%"
+      height="auto"
+    />
+  );
+};
+
+export default VideoPlayer;
